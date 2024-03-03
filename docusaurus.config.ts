460e84f6b882ab78f8a5d9254f23fa24abce3f74
Joshua Castle <26531652+Kas-tle@users.noreply.github.com>
2024-03-03 12:07:43 -0800

    Specify site image
    Signed-off-by: Joshua Castle <26531652+Kas-tle@users.noreply.github.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,7 +52,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
+        image: 'img/site/geyser.png',
         navbar: {
             title: 'GeyserMC',
             logo: {
